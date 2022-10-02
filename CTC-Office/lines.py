36596028cf11ec708eb94fe9ebc16c3fe0a2a6a0
Garrett Marcinak <garrettmarcinak@gmail.com>
2022-10-02 11:43:33 -0400

    Add CTC Office starter files

---
+++ CTC-Office/lines.py
@@ -0,0 +1,26 @@
+import sys
+
+class station:
+    stop = False
+
+    def __init__(self, name):
+        self.name = name
+    
+    def setAsDestination():
+        stop = True
+    
+    def removeAsDestination():
+        stop = False
+
+# define red line
+redLine = []
+redLine.append(station('SHADYSIDE'))
+redLine.append(station('HERRON AVE'))
+redLine.append(station('SWISSVALE'))
+redLine.append(station('PENN STATION'))
+redLine.append(station('STEEL PLAZA'))
+redLine.append(station('FIRST AVE'))
+redLine.append(station('SOUTH HILLS JUNCTION'))
+
+
+
---
+++ CTC-Office/train.py
@@ -0,0 +1,25 @@
+import sys
+from turtle import speed
+
+class train:
+    def __init__(self):
+        print("Train instantiated")
+    
+    def setAuthority(newAuthority):
+        authority = newAuthority
+
+    def commandSpeed(newSpeed):
+        print("Commanding speed for train: ", newSpeed, "mph")
+
+    ## def getSpeed():
+
+    ## def getAuthority():
+    
+    #def addDestinations(newDestinations):   
+        
+    ## TODO: add list of destinations
+
+    ## TODO: add arrival times
+
+
+
