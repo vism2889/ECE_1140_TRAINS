b58b3b76537ea5e68c5e5c84e1c903c1c55ec0ac
juinsommer <juinchoisommer@gmail.com>
2022-10-11 14:40:06 -0400

    most recent push

---
+++ trainController_hw/pot_test.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+pin_a = 18
+pin_b = 24
+
+def discharge():
+    GPIO.setup(pin_a, GPIO.IN)
+    GPIO.setup(pin_b, GPIO.OUT)
+    GPIO.output(pin_b, False)
+    time.sleep(0.004)
+
+def charge_time():
+    GPIO.setup(pin_b, GPIO.IN)
+    GPIO.setup(pin_a, GPIO.OUT)
+    count = 0
+    GPIO.output(pin_a, True)
+    while not GPIO.input(pin_b):
+        count += 1
+    return count
+
+def analog_read():
+    discharge()
+    return charge_time()
+
+while True:
+    print(analog_read())
+    time.sleep(1)
