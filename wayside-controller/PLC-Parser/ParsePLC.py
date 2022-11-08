3064150f9b2c5c662517e7001e6170bd2d9d8851
NMMallick <nmm109@pitt.edu>
2022-11-08 12:07:58 -0500

    working on parser

--- wayside-controller/PLC-Parser/ParsePLC.py
+++ wayside-controller/PLC-Parser/ParsePLC.py
@@ -1,8 +1,65 @@
 import os 
+import re
+
+class PLCParser():
+    def __init__(self):
+        self.IO_KEYWORDS = ["block_", "crossing_"]
+        self.parsed_instructions = []
+        pass
+
+    def parseFile(self, file):  
+        ## Parse each instruction in a file
+        for instr in file:
+            
+            instruction = {}
+            
+            ## Split the instruction based on ':='
+            tokens = re.split(r':=|if|else', instr)
+
+            if len(tokens) == 3 or len(tokens) == 4:
+                
+                ## Get the output variable
+                output_var = tokens[0]
+                
+                if '#' not in output_var:
+                    continue
+                else:
+                    instruction['output'] = output_var.strip("#")
+                                
+                ## Get conditional assignment value
+                if tokens[1].lower() != 'true' or tokens[1].lower() != 'false':
+                    continue
+                else:
+                    conditional_val = True if tokens[1].strip(' ').lower() == 'true' else False
+                    instruction['value'] = conditional_val
+
+                ## Get conditional expression
+                conditional_expr = tokens[2]
+                
+
+                ## Default value (if present)
+                if len(tokens) == 4:
+                    default_val = True if tokens[3].strip(' ').lower() == 'true' else False
+                    instruction['default'] = default_val
+
+                self.parsed_instructions.append(instruction)
+            
+                
+                
+                
+
+            
+            
 
 
-def parseString(file):
+
+
+if __name__ == '__main__':
     
+    parser = PLCParser()
+    path = os.getcwd() + "\\tests\\testplc.plc"
+    file = open(path, 'r')
 
+    parser.parseFile(file)
 
-    
+    file.close()
--- wayside-controller/WaysideIO/WaysideIO.py
+++ wayside-controller/WaysideIO/WaysideIO.py
@@ -8,27 +8,17 @@ class WaysideIO:
 
         pass
 
-
-
     def configurePorts(self, file):
         
         ## Loop through each instruction
         for line in file:
-            ## Split the line at ','
-            instr = line.split(',')
-            
-            if len(instr) <= 3:
-                continue
-            
-            ## Go through each element of the instruction
-            #   (i.e. [AND, 1, 2, 50]) AND ports 1 and 2 and
-            #   set that output to block 50 
-            op = instr[0]
-            output = instr[-1]
-            inputs = []
-            for el in instr[1:len(instr)-1]:
-                inputs 
-
-
-
+            if line[:2] == 'if':
+                print("if")
+    
         pass
+
+
+if __name__ == '__main__':
+    w = WaysideIO()
+    
+    
