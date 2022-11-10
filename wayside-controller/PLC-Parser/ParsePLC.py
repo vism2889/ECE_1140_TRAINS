2ee8cfc5e6eb8ddddf947c0044c9e3ea9f45446a
NMMallick <nmm109@pitt.edu>
2022-11-09 19:18:17 -0500

    finishing up parser

--- wayside-controller/PLC-Parser/ParsePLC.py
+++ wayside-controller/PLC-Parser/ParsePLC.py
@@ -1,65 +1,57 @@
-import os 
+import os
 import re
+import sys
+from copy import deepcopy
 
 class PLCParser():
-    def __init__(self):
-        self.IO_KEYWORDS = ["block_", "crossing_"]
-        self.parsed_instructions = []
-        pass
+    def __init__(self, controllerId):
 
-    def parseFile(self, file):  
-        ## Parse each instruction in a file
-        for instr in file:
-            
-            instruction = {}
-            
-            ## Split the instruction based on ':='
-            tokens = re.split(r':=|if|else', instr)
+        self.VAR_TYPES = ['#block_', '#switch_', '#crossing_']
 
-            if len(tokens) == 3 or len(tokens) == 4:
-                
-                ## Get the output variable
-                output_var = tokens[0]
-                
-                if '#' not in output_var:
-                    continue
-                else:
-                    instruction['output'] = output_var.strip("#")
-                                
-                ## Get conditional assignment value
-                if tokens[1].lower() != 'true' or tokens[1].lower() != 'false':
-                    continue
-                else:
-                    conditional_val = True if tokens[1].strip(' ').lower() == 'true' else False
-                    instruction['value'] = conditional_val
+        dir = None
+        if os.name == 'posix':
+            dir = '/plc'
+        elif os.name == 'nt':
+            dir = '\\plc'
 
-                ## Get conditional expression
-                conditional_expr = tokens[2]
-                
+        plcDir = os.path.join(os.getcwd() + dir) if dir != None else None
 
-                ## Default value (if present)
-                if len(tokens) == 4:
-                    default_val = True if tokens[3].strip(' ').lower() == 'true' else False
-                    instruction['default'] = default_val
+        if plcDir != None and not os.path.exists(plcDir):
+            os.mkdir(plcDir)
 
-                self.parsed_instructions.append(instruction)
-            
-                
-                
-                
+        self.outfilePath = ""
 
-            
-            
+        if os.name == 'posix':
+            self.outfilePath = os.getcwd() + f'{dir}/controller_plc_{controllerId}.py'
+        elif os.name == 'nt':
+            self.outfilePath = os.getcwd() + f'{dir}\\controller_plc_{controllerId}.py'
 
+    def parseFile(self, file):
 
+        outfile = open(self.outfilePath, 'w')
+        lines = []
 
+        for line in file:
+            for vtype in self.VAR_TYPES:
+                idx = line.find(vtype)
+                while idx>=0:
+                    num = line[idx:].split(' ')[0].replace(vtype, '')
+                    line = line.replace(vtype+num, f"input['{vtype.replace('#', '').replace(f'_', '')}']['{num}']")
+                    idx = line.find(vtype)
+            lines.append(line)
+
+        print(lines)
 
 if __name__ == '__main__':
-    
-    parser = PLCParser()
-    path = os.getcwd() + "\\tests\\testplc.plc"
+
+    parser = PLCParser(1)
+    path = os.getcwd()
+
+    if os.name == 'nt':
+        path += "\\tests\\testplc.plc"
+    elif os.name == 'posix':
+        path += "/tests/testplc.plc"
+
     file = open(path, 'r')
-
     parser.parseFile(file)
-
     file.close()
