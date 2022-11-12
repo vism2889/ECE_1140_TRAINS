1d91504fda0e3d641867eb3002666a5382ecc92a
garrett <garrettmarcinak@gmail.com>
2022-11-12 16:03:01 -0500

    Reorganize files

--- CTC-Office/train-functionality/Train.py
+++ CTC-Office/train-functionality/Train.py
@@ -2,10 +2,11 @@ import sys
 
 class Train:
     
-    def __init__(self, destinations):
-        self.destinations = destinations
-        self.commandedSpeed = "None"
-        self.authority = "None"
+    def __init__(self, location, destinations, commandedSpeed, authority):
+        self.destinations = destinations.copy()
+        self.commandedSpeed = commandedSpeed
+        self.authority = authority
+        self.location = "YARD"
 
 
     
