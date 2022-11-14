f1a69c70195db180375c75afb0ab79f3506e8ff6
NMMallick <nmm109@pitt.edu>
2022-11-14 01:45:05 -0500

    ...

--- wayside-controller/WaysideIO/WaysideIO.py
+++ wayside-controller/WaysideIO/WaysideIO.py
@@ -8,17 +8,41 @@ from track_layout import extract_layout
 class Controller:
     def __init__(self, controllerNum, layout):
 
+        self.layout = layout
         ## Function to run PLC program
         self.plc = None
         self.plcGood = False
 
-        self.parser = PLCParser(controllerNum)
+        ##
+        self.maintenance = True
+        # print(f'Controller {controllerNum}')
+        # print(layout)
 
+        ## Setup layout
+
+        self.track = {
+            'blocks' : {}, 
+            'switches' : {},
+            'crossings': {}
+        }
+
+        for section in self.layout['sections']:
+            ## Blocks
+            for block in self.layout['sections'][section]['blocks']:
+                self.track['blocks'][block[0]] = block[1]
+
+            ## Switches
+            for switch in self.layout['sections'][section]['switches']:
+                self.track['switches'][switch] = False
+
+            ## Crossings
+            for crossing in self.layout['sections'][section]['crossing']:
+                self.track['crossings'][crossing] = False
+ 
+        self.parser = PLCParser(controllerNum)
+ 
     ## Run the PLCs
     def run(self):
-        input = {
-            'switch'
-        }
         if self.plcGood:
             try:
                 self.plc(self.track)
@@ -55,15 +79,24 @@ class WaysideIO:
             'green' : {}
         }
 
-    def updateBlock(self,line, blockNum):
-        pass
+    def lookupBlock(self, line, blockNum):
+        return self.lookupTable[line.lower()][str(blockNum)]
+
+    def uploadPLC(self, line, controllerNum, file):
+        ## Redline
+        if line.lower() == self.lines[0]:
+            self.redline_controllers[controllerNum].uploadPLC(file)
+        
+        ## Greenline
+        if line.lower() == self.lines[1]:
+            self.greenline_controllers[controllerNum].uploadPLC(file)
+
 
     def setupLine(self, line, layout):
-
         ## Redline
         if line.lower() == self.lines[0]:
             for i, c in enumerate(layout):
-                self.redline_controllers.append(c)
+                self.redline_controllers.append(Controller(i, c))
 
                 ## Populate lookup table
                 for sec in c['sections']:
@@ -80,7 +113,7 @@ class WaysideIO:
         ## Greenline
         if line.lower() == self.lines[1]:
             for i, c in enumerate(layout):
-                self.greenline_controllers.append(c)
+                self.greenline_controllers.append(Controller(i, c))
 
                 ## Populate lookup table
                 for sec in c['sections']:
@@ -93,7 +126,7 @@ class WaysideIO:
 
                         entry[block[0]]['controller'].append(i)
                         entry[block[0]]['section'] = sec
-        print(self.lookupTable)
+    
 
 if __name__ == '__main__':
     w = WaysideIO(1)
@@ -115,3 +148,5 @@ if __name__ == '__main__':
 
     *other, layout = extract_layout.parseTrackLayout(csvPath, jsonPath)
     w.setupLine('green', layout)
+
+
