b274eba166f06487cef37eeee203e1fc2804ba8f
NMMallick <nmm109@pitt.edu>
2022-12-15 15:59:59 -0500

    fixing plc logic for the redline

--- Pittsburgh-Light-Rail-App/plc/red/controller_plc_2.py
+++ Pittsburgh-Light-Rail-App/plc/red/controller_plc_2.py
@@ -1,10 +1,15 @@
 def run(input):
 	if input['block'][45] == True or input['block'][46] == True or input['block'][47] == True or input['block'][48] == True or input['block'][49] == True:
 	    input['crossing'][47] = True
-	else: 
+	else:
 	    input['crossing'][47] = False
 	
 	input['switch'][27] = True
 	input['switch'][32] = True
 	input['switch'][38] = True
 	input['switch'][43] = True
+	
+	if input['block'][50] == True or input['block'][51] == True:
+	    input['switch'][52] = True
+	elif input['block'][53] == True or input['block'][54] == True:
+	    input['switch'][52] = False
