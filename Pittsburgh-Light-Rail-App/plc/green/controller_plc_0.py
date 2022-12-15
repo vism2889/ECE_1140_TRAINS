964ff6133b2448b32638c7e95afa59667e7ae185
NMMallick <nmm109@pitt.edu>
2022-12-14 20:26:49 -0500

    plc logic tuning for red and greenline, synced switch positions

--- Pittsburgh-Light-Rail-App/plc/green/controller_plc_0.py
+++ Pittsburgh-Light-Rail-App/plc/green/controller_plc_0.py
@@ -1,13 +1,18 @@
 def run(input):
-	if input['block'][18] == True or input['block'][19] == True or input['block'][20] == True:
+	if input['block'][18] == True or input['block'][19] == True or input['block'][20] == True or input['block'][21] == True:
 	    input['crossing'][19] = True
 	else:
 	    input['crossing'][19] = False
 	
-	if (input['block'][148] == True or input['block'][149] == True) and input['block'][3] == False:
-	    input['switch'][29] = True
+	occupied = False
+	for blk in range(13, 30):
+	    if input['block'][blk] == True:
+	        occupied == True 
+	
+	if (input['block'][150] == True or input['block'][149] == True) and not occupied:
+	    input['switch'][29] = False
 	    input['switch'][12] = True
 	
-	if (input['block'][3] == True  or input['block'][2] == True) and input['block'][149] == False:
-	    input['switch'][29] = False
+	if (input['block'][1] == True  or input['block'][2] == True) and not occupied:
+	    input['switch'][29] = True
 	    input['switch'][12] = False
